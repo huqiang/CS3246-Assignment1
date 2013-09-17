ae1c8e6d9d408635628a4b67f79c8b656657fcf6
Hu Qiang <nickhuq@gmail.com>
2013-09-17 16:50:14 +0800

    Added similarities method

--- SearchFiles.py
+++ SearchFiles.py
@@ -11,15 +11,10 @@ from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.store import SimpleFSDirectory
 from org.apache.lucene.search import IndexSearcher
 from org.apache.lucene.util import Version
+import org.apache.lucene.search.similarities as similarities
 
 """
-This script is loosely based on the Lucene (java implementation) demo class 
-org.apache.lucene.demo.SearchFiles.  It will prompt for a search query, then it
-will search the Lucene index in the current directory called 'index' for the
-search query entered against the 'contents' field.  It will then display the
-'path' and 'name' fields for each of the hits it finds in the index.  Note that
-search.close() is currently commented out because it causes a stack overflow in
-some cases.
+
 """
 def run(searcher, analyzer):
     while True:
@@ -47,6 +42,9 @@ if __name__ == '__main__':
     base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
     directory = SimpleFSDirectory(File(os.path.join(base_dir, INDEX_DIR)))
     searcher = IndexSearcher(DirectoryReader.open(directory))
+    
+    searcher.setSimilarity(similarities.BM25Similarity())
+    #Available similarity: BM25Similarity, MultiSimilarity, PerFieldSimilarityWrapper, SimilarityBase, TFIDFSimilarity
     analyzer = StandardAnalyzer(Version.LUCENE_CURRENT)
     run(searcher, analyzer)
     del searcher
